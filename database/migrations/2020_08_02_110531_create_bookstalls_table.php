7eea39c9822db23681822a8f92e358fe4dd1d894
sudipto <sudiptoshil@outlook.com>
2020-08-05 13:09:54 +0600

    client api auth added

---
+++ database/migrations/2020_08_02_110531_create_bookstalls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookstallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookstalls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('admin_id')->unsigned();
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->tinyinteger('activity')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookstalls');
+    }
+}
