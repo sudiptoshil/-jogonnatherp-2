1979a69e684a96c84c7248ae8a021e58f5c4eb5a
sudipto <sudiptoshil@outlook.com>
2020-08-10 17:23:44 +0600

    bakery authentication and management added

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -90,29 +90,8 @@
                    <!-- END THEME CUSTOMIZER-->
                   <!-- BEGIN PAGE TITLE & BREADCRUMB-->
                    <h3 class="page-title">
-                     Dashboard
+                    Admin Dashboard
                    </h3>
-                   <ul class="breadcrumb">
-                       <li>
-                           <a href="#">Home</a>
-                           <span class="divider">/</span>
-                       </li>
-                       <li>
-                           <a href="#">Metro Lab</a>
-                           <span class="divider">/</span>
-                       </li>
-                       <li class="active">
-                           Dashboard
-                       </li>
-                       <li class="pull-right search-wrap">
-                           <form action="http://thevectorlab.net/metrolab/search_result.html" class="hidden-phone">
-                               <div class="input-append search-input-area">
-                                   <input class="" id="appendedInputButton" type="text">
-                                   <button class="btn" type="button"><i class="icon-search"></i> </button>
-                               </div>
-                           </form>
-                       </li>
-                   </ul>
                    <!-- END PAGE TITLE & BREADCRUMB-->
                </div>
             </div>
--- resources/views/bookstall/master.blade.php
+++ resources/views/bookstall/master.blade.php
@@ -88,29 +88,9 @@
                    <!-- END THEME CUSTOMIZER-->
                   <!-- BEGIN PAGE TITLE & BREADCRUMB-->
                    <h3 class="page-title">
-                     Dashboard
+                     Bookstall Dashboard
                    </h3>
-                   <ul class="breadcrumb">
-                       <li>
-                           <a href="#">Home</a>
-                           <span class="divider">/</span>
-                       </li>
-                       <li>
-                           <a href="#">Metro Lab</a>
-                           <span class="divider">/</span>
-                       </li>
-                       <li class="active">
-                           Dashboard
-                       </li>
-                       <li class="pull-right search-wrap">
-                           <form action="http://thevectorlab.net/metrolab/search_result.html" class="hidden-phone">
-                               <div class="input-append search-input-area">
-                                   <input class="" id="appendedInputButton" type="text">
-                                   <button class="btn" type="button"><i class="icon-search"></i> </button>
-                               </div>
-                           </form>
-                       </li>
-                   </ul>
+                   
                    <!-- END PAGE TITLE & BREADCRUMB-->
                </div>
             </div>
